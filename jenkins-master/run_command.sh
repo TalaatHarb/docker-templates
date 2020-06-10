a1320ec62df56e5c4c3116a65df888dfea404774
TalaatHarb <eng.talat.harb@gmail.com>
2020-06-10 13:14:52 +0000

    Fixed name of volume mount

--- jenkins-master/run_command.sh
+++ jenkins-master/run_command.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 --name jenkins-master -v ./jenkins_home:/var/jenkins_home -d talaatharb/jenkins-master
+docker run -p 8080:8080 --name jenkins-master -v jenkins_home:/var/jenkins_home -d talaatharb/jenkins-master
